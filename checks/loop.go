30b5a5accf3ab66d9a1524e13e56e7f883a642c8
daniel1302 <daniel.1302@gmail.com>
2023-12-12 00:47:03 +0100

    feat: add more logging to health-check and execute ititial check just after servise is started

--- checks/loop.go
+++ checks/loop.go
@@ -15,20 +15,30 @@ func HealthCheckLoop(ctx context.Context, resultChan chan<- Result, checks []Hea
 
 	tick := time.NewTicker(checkInterval)
 
+	checkExecution := func(resultChan chan<- Result, checks []HealthCheckFunc) {
+		result := Result{
+			Status:  StatusHealthy,
+			Reasons: []error{},
+		}
+		for _, checkFunc := range checks {
+			if err := checkFunc(); err != nil {
+				result.Reasons = append(result.Reasons, err)
+				result.Status = StatusUnhealthy
+
+				log.Printf("Endpoint unhealthy because of %s", err.Error())
+			}
+		}
+		resultChan <- result
+
+		log.Print("Health check loop execution finished")
+	}
+
+	checkExecution(resultChan, checks)
+
 	for {
 		select {
 		case <-tick.C:
-			result := Result{
-				Status:  StatusHealthy,
-				Reasons: []error{},
-			}
-			for _, checkFunc := range checks {
-				if err := checkFunc(); err != nil {
-					result.Reasons = append(result.Reasons, err)
-					result.Status = StatusUnhealthy
-				}
-			}
-			resultChan <- result
+			checkExecution(resultChan, checks)
 
 		case <-ctx.Done():
 			log.Printf("Health check loop stopped due to context done")
